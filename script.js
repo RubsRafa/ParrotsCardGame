70bd992d690d7594138fda8ff416a18db2137913
RubsRafa <rubiarafaela2000@gmail.com>
2022-10-21 17:08:58 -0300

    versão completa sem bonus

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-//let qtdCartas = prompt ("Com quantas cartas você deseja jogar? Números pares de 4 a 14.");
-//while (qtdCartas != 4 && qtdCartas != 6 && qtdCartas != 8 && qtdCartas != 10 && qtdCartas != 12 && qtdCartas != 14 ) {
-//    alert ("Quantidade de cartas inválidas!");
-//    qtdCartas = prompt ("Com quantas cartas você deseja jogar? Números pares de 4 a 14.");
-//}
+alert ("Bem-vindo ao jogo do piriquito que não é o Duolingo!");
+const nome = prompt ("Qual é o seu nome");
+
+let qtdCartas = prompt ("Com quantas cartas você deseja jogar? Números pares de 4 a 14.");
+while (qtdCartas != 4 && qtdCartas != 6 && qtdCartas != 8 && qtdCartas != 10 && qtdCartas != 12 && qtdCartas != 14 ) {
+    alert ("Quantidade de cartas inválidas!");
+    qtdCartas = prompt ("Com quantas cartas você deseja jogar? Números pares de 4 a 14.");
+}
+
+qtdCartas = Number(qtdCartas);
 
 
-const piriquitos = [`bobross`, `explody`, `fiesta`, `metal`, `revertit`, `triplets`, `unicorn`]
+const piriquitos = ['bobross', 'bobross', 'explody', 'explody', 'fiesta', 'fiesta', 'metal', 'metal', 'revertit', 'revertit', 'triplets', 'triplets', 'unicorn', 'unicorn']
+const piriquitosProJogo = [];
 
-const piriquitosDuplicados = [ ... piriquitos, ... piriquitos];
-let piriquitosEmbaralhados = piriquitosDuplicados.sort (embaralhar); 
-
-function embaralhar () {
-    Math.random() - 0.5; 
+for (let indice = 0; indice < qtdCartas; indice++) {
+    piriquitosProJogo.push (piriquitos[indice]);
 }
 
 
+function misturar () {
+    return Math.random () - 0.5; 
+}
+
+const piriquitosEmbaralhados = piriquitosProJogo.sort (misturar);
 
 
-qtdCartas = Number (qtdCartas);
 const addCartas = document.querySelector (".cartas");
 for (let i = 0; i < qtdCartas; i++) {
     addCartas.innerHTML += `<li><div class="carta" onclick="virar(this);">
@@ -33,24 +40,35 @@ let segundaCarta;
 let nomePrimeiraCarta;
 let nomeSegundaCarta; 
 
+
+let contador = 0;
 function virar(elemento){
     if (primeiraCarta == undefined) {
         elemento.classList.add ("virada");
         primeiraCarta = elemento;
         nomePrimeiraCarta = elemento.querySelector ("h1"); 
+        contador++; 
     } else if (segundaCarta == undefined) {
         elemento.classList.add ("virada");
         segundaCarta = elemento; 
-        nomeSegundaCarta = elemento.querySelector ("h1"); 
+        nomeSegundaCarta = elemento.querySelector ("h1");
+        contador++;  
     }
     compararCartas(); 
 
 
 }
-
+let contadorCartas = 0;
 function compararCartas () {
-    if (nomePrimeiraCarta.innerHTML == nomeSegundaCarta.innerHTML) {
-        
+    let compararCarta1 = nomePrimeiraCarta.innerHTML; 
+    let compararCarta2 = nomeSegundaCarta.innerHTML; 
+    if (compararCarta1 == compararCarta2) {
+        primeiraCarta.classList.add ("acertou"); 
+        segundaCarta.classList.add ("acertou");
+
+        contadorCartas += 2;
+        console.log (contadorCartas);
+
         primeiraCarta = undefined;
         segundaCarta = undefined;
         nomePrimeiraCarta = undefined;
@@ -66,13 +84,14 @@ function compararCartas () {
         segundaCarta = undefined;
         nomePrimeiraCarta = undefined;
         nomeSegundaCarta = undefined; 
-        }, 800)
+        }, 500)
     }
 }
 
 function venceu () {
-    let cartasViradas = document.querySelectorAll (".carta");
-    if (cartasViradas.classList.contains("virada")) {
-        prompt ("Parabéns! Você venceu!")
+    if (contadorCartas === qtdCartas) {
+        alert (`Parabéns, ${nome} mizerávi! Você arrasou no jogo do piriquito, com ${contador} jogadas.`); 
     }
 }
+
+setTimeout (contadorCartas, )
